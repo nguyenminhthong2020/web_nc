73837fa30fe34540aabab097e160250942ae70e3
juernevte <1612674@student.hcmus.edu.vn>
2020-07-03 23:38:52 +0700

    fix bug

--- backend_mongodb/utils/db.js
+++ backend_mongodb/utils/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbConnect = mongoose.connect(process.env.MONGODB_URL, {
+const dbConnect = mongoose.connect(process.env.DATABASE_URL, {
     // useNewUrlParser: true,
     // useCreateIndex: true,
 
